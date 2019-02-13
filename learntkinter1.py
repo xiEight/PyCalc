61c5910caaab26cbde71f1e5c14910744e00f15e
xiEight <45069472+xiEight@users.noreply.github.com>
2019-02-13 23:41:39 +0300

    Create learntkinter1.py
    Main code

---
+++ learntkinter1.py
@@ -0,0 +1,74 @@
+from tkinter import*
+def add():
+    global x
+    global y
+    try:
+        xx = int(x.get())
+        yy = int(y.get())
+        s = str(xx + yy)
+    except ValueError:
+        s = "Value must be integers!"
+    finally:
+        l['text'] = s
+        l.pack()
+
+def mins():
+    global x
+    global y
+    try:
+        xx = int(x.get())
+        yy = int(y.get())
+        s = str(xx - yy)
+    except ValueError:
+        s = "Value must be integers!"
+    finally:
+        l['text'] = s
+        l.pack()
+
+def mult():
+    global x
+    global y
+    try:
+        xx = int(x.get())
+        yy = int(y.get())
+        s = str(xx * yy)
+    except ValueError:
+        s = "Value must be integers!"
+    finally:
+        l['text'] = s
+        l.pack()
+
+def divz():
+    global x
+    global y
+    global l
+    try:
+        xx = int(x.get())
+        yy = int(y.get())
+        s = str(xx / yy)
+    except ValueError:
+        s = "Введены должны быть только числа!"
+    except ZeroDivisionError:
+        s = "Деление на 0!"
+    finally:
+        l['text'] = s
+        l.pack()
+
+
+
+   
+root = Tk()
+x = Entry(justify = "center",width = 30)
+y = Entry(justify = "center",width = 30)
+pls = Button(text = '+', command = add,width = 30)
+mns = Button(text = '-', command = mins,width = 30)
+mul = Button(text = '*', command = mult,width = 30)
+dv = Button(text = '/', command = divz,width = 30)
+x.pack()
+y.pack()
+dv.pack()
+mul.pack()
+mns.pack()
+pls.pack()
+l = Label()
+root.mainloop()
